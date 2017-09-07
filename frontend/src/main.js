397f7d61f436091f2ab5caf613ae99f863ceffe0
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-09-07 14:01:54 -0700

    Provider now contains App in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Provider} from 'react-redux'
+
+import appCreateStore from './lib/app-create-store.js'
 import App from './component/app';
 
-ReactDom.render(<App />, document.getElementById('root'))
+let store = appCreateStore();
+
+let AppContainer = () => {
+  return(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+ReactDom.render(<AppContainer />, document.getElementById('root'))
