24e560f6cfc788fe3d8052a4f527f4a8381d6169
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-09-07 14:08:41 -0700

    started work on a and dashboard, but realized i need a profile form first

--- frontend/src/component/app/index.js
+++ frontend/src/component/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import appCreateStore from '../../lib/app-create-store.js';
 import LandingContainer from '../landing-container';
 import DashboardContainer from '../dashboard-container';
 
@@ -11,23 +10,21 @@ class App extends React.Component {
   render() {
     return(
       <div className='cfGram'>
-        <Provider store={store}>
-          <BrowserRouter>
-            <section>
-              <header>
-                <h1>cfgram</h1>
-                <nav>
-                  <ul>
-                    <li><Link to='/welcome/signup'>signup</Link></li>
-                    <li><Link to='/welcome/login'>login</Link></li>
-                  </ul>
-                </nav>
-              </header>
-              <Route path='/welcome/:auth' component={LandingContainer} />
-              <Route exact path='/welcome' component={DashboardContainer} />
-            </section>
-          </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+          <section>
+            <header>
+              <h1>cfgram</h1>
+              <nav>
+                <ul>
+                  <li><Link to='/welcome/signup'>signup</Link></li>
+                  <li><Link to='/welcome/login'>login</Link></li>
+                </ul>
+              </nav>
+            </header>
+            <Route path='/welcome/:auth' component={LandingContainer} />
+            <Route exact path='/welcome' component={DashboardContainer} />
+          </section>
+        </BrowserRouter>
       </div>
     )
   }
