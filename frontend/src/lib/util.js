6e2180faeea1d7d193ff16eee6a9b11c89d1a641
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-09-07 13:57:21 -0700

    added photoToDataUrl and readCookie to util.js

--- frontend/src/lib/util.js
+++ frontend/src/lib/util.js
@@ -1,3 +1,33 @@
+export const photoToDataURL = (file) => {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.addEventListener('load', () => {
+      resolve(reader.result);
+    });
+    reader.addEventListener('error', () => {
+      reject(reader.error);
+    });
+    if(file) {
+      return reader.readAsDateURL(file)
+    }
+    return reject(new Error('USAGE ERROR: requires file'))
+  })
+}
+
+//got from:
+//https://stackoverflow.com/questions/14573223/set-cookie-and-get-cookie-with-javascript
+
+export const readCookie = (name) => {
+  var nameEQ = name + =;
+  var ca = document.cookie.split(';');
+  for(let i=0; i < ca.length; i++) {
+    let c = ca[i];
+    while(c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if(c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
 export const log = (...args) =>
   __DEBUG__ ? console.log(...args) : undefined;
 
