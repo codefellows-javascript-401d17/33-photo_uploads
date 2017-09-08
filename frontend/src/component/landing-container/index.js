376772172dfd4bfb1a085b90f86aef41a774be4d
Allan Liebold <allan.liebold@gmail.com>
2017-09-07 17:15:32 -0700

    adds frontend from lab 32

---
+++ frontend/src/component/landing-container/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import AuthForm from '../auth-form';
+import * as util from '../../lib/util.js';
+
+class LandingContainer extends React.Component {
+  render() {
+    let {params} = this.props.match;
+
+    let handleComplete = params.auth === 'login'
+      ? this.props.login
+      : this.props.signup
+
+    return (
+      <div>
+        <AuthForm
+          auth={params.auth}
+          onComplete={handleComplete} />
+      </div>
+    )
+  }
+}
+
+let mapDispatchToProps = (dispatch) => {
+  return {
+    signup: (user) => dispatch(signRequest(user)),
+    login: (user) => dispatch(loginRequest(user))
+  }
+}
+
+export default connect(undefined, mapDispatchToProps)(LandingContainer);
